5b65ec865a4719dc80d5de2fdd6c4254021c4427
Jesse <jessebot.git@gmail.com>
2023-03-20 15:02:36 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import PageLayout from "@/components/page-layout"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faList } from "@fortawesome/free-solid-svg-icons"
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -18,10 +19,10 @@ export default function Home() {
         </div>
         <ul className="index-socials">
           <li className="index-socials-item">
-            <a href="/blog" className="index-socials-item-link"><FontAwesomeIcon icon={faList} /> Blog</a>
+            <Link href="/blog" className="index-socials-item-link"><FontAwesomeIcon icon={faList} /> Blog</Link>
           </li>
           <li className="index-socials-item">
-            <a href="https://github.com/JessebotX" className="index-socials-item-link"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> JessebotX</a>
+            <Link href="https://github.com/JessebotX" className="index-socials-item-link"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> JessebotX</Link>
           </li>
         </ul>
       </div>
